fcb0481a227924b61dd30f17c89deccfba4dd400
real-na <naogko@163.com>
2020-11-12 20:49:07 +0800

    cancel redux

--- mcake/src/index.js
+++ mcake/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import {HashRouter,BrowserRouter} from 'react-router-dom';
 import App from './App';
 // import './assets/iconfont/iconfont.css'
-import {Provider} from 'react-redux'
 
 const Router = process.env.NODE_ENV === 'production' ? BrowserRouter : HashRouter
 
 ReactDOM.render(
-    // <Provider store={store}>
         <Router>
             <App/>
         </Router>
-    //  </Provider>
     ,
     document.querySelector('#root')
 )
