9a02f107081204522f6f4a1afad52ac42b7e98c8
real-na <naogko@163.com>
2020-11-12 20:33:02 +0800

    details

--- mcake/src/components/Home/Banner.jsx
+++ mcake/src/components/Home/Banner.jsx
@@ -10,8 +10,8 @@ export default class Banner extends React.Component {
           infinite
           vertical ={false}
           dots={true}
-          dotStyle={{margin:'0 2vw',display:'inline-block',height:10,width:10,borderRadius:50+"%",backgroundColor:'rgba(222,222,222,.3)'}}
-          dotActiveStyle={{margin:'0 2vw',display:'inline-block',height:10,width:10,borderRadius:50+"%",backgroundColor:'rgba(121,21,21,.5)'}}
+          /* dotStyle={{margin:'0 2vw',display:'inline-block',height:10,width:10,borderRadius:50+"%",backgroundColor:'rgba(222,222,222,.3)'}}
+          dotActiveStyle={{margin:'0 2vw',display:'inline-block',height:10,width:10,borderRadius:50+"%",backgroundColor:'rgba(121,21,21,.5)'}} */
         >
           {this.props.Bannerlist.map((item,index) => (
             <a
